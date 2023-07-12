b94c5fad79e845d5aff9505d36cf89505232d5de
Ernest Magriñá <ernest8081@gmail.com>
2023-07-12 04:47:21 +0200

    refactor: 🐛 Change title app

--- src/app/modules/characters/components/card-character/card-character.component.ts
+++ src/app/modules/characters/components/card-character/card-character.component.ts
@@ -3,8 +3,14 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-card-character',
   templateUrl: './card-character.component.html',
-  styleUrls: ['./card-character.component.css']
+  styleUrls: ['./card-character.component.css'],
 })
 export class CardCharacterComponent {
   @Input() character: any;
+
+  isImageLoaded: boolean = false;
+
+  handleImageLoad() {
+    this.isImageLoaded = true;
+  }
 }
